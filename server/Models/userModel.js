5035e739d09e9008940b7d769c4c597bc9efaf9e
HarshitPG <harshitpg2004@gmail.com>
2023-09-29 15:04:57 +0530

    selection updates

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -1,41 +1,45 @@
 import mongoose from "mongoose";
 
 const UserSchema = mongoose.Schema(
-    {
-        username: {                 // username is email
-            type: String,
-            required: true
-        },
-        password : {
-            type: String,
-            required: true
-        },
-        firstname: {
-            type: String,
-            required: false
-        },
-        lastname : {
-            type: String,
-            required: false
-        },
-        gender: String,
-        mobile : {
-            type: Number,
-            required: false
-        },
-        isAdmin : {
-            type: Boolean,
-            default: false,
-        },
-        profilePicture: String,
-        coverPicture: String,
-        about: String,
-        followers: [],
-        following: [],
-        likesRoom: []
+  {
+    username: {
+      // username is email
+      type: String,
+      required: true,
     },
-    {timestamps: true}
-)
+    password: {
+      type: String,
+      required: true,
+    },
+    firstname: {
+      type: String,
+      required: false,
+    },
+    lastname: {
+      type: String,
+      required: false,
+    },
+    gender: {
+      type: String,
+      required: false,
+    },
+    mobile: {
+      type: Number,
+      required: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    profilePicture: String,
+    coverPicture: String,
+    about: String,
+    followers: [],
+    following: [],
+    likesRoom: [],
+  },
+  { timestamps: true }
+);
 
-const UserModel= mongoose.model("Users", UserSchema);
-export default UserModel
+const UserModel = mongoose.model("Users", UserSchema);
+export default UserModel;
