2c89e9e15cf21d8b4f9be6ba935496eb8def3b34
utkarsh-creator <utkarshtyagi959@gmail.com>
2023-10-31 16:04:38 +0530

    Minor updates

--- client/src/Pages/UpdatePassword/UpdatePassword.jsx
+++ client/src/Pages/UpdatePassword/UpdatePassword.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import "./UpdatePassword.css";
+import "react-toastify/dist/ReactToastify.css";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
