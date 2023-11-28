e1ea147622cc4f6fa6eb2482ea37b339e194e1d5
Subhodeep Dey <sdeysocial@gmail.com>
2023-11-28 20:54:11 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -109,6 +109,35 @@ if(process.env.NODE_ENV === "production") {
   console.log = () => {}
   console.error = () => {}
   console.debug = () => {}
+  console.warn = () => {}
+  console.info = () => {}
+  console.trace = () => {}
+  console.group = () => {}
+  console.groupEnd = () => {}
+  console.groupCollapsed = () => {}
+  console.time = () => {}
+  console.timeEnd = () => {}
+  console.timeLog = () => {}
+  console.count = () => {}
+  console.countReset = () => {}
+  console.clear = () => {}
+  console.table = () => {}
+  console.assert = () => {}
+  console.profile = () => {}
+  console.profileEnd = () => {}
+  console.dir = () => {}
+  console.dirxml = () => {}
+  console.timeStamp = () => {}
+  console.context = () => {}
+  console.memory = () => {}
+  console.exception = () => {}
+  console.trace = () => {}
+  console.fatal = () => {}
+  console.markTimeline = () => {}
+  console.timeline = () => {}
+  console.timelineEnd = () => {}
+  console.count = () => {}
+  console.countReset = () => {}
 }
 
 const router = createBrowserRouter([
