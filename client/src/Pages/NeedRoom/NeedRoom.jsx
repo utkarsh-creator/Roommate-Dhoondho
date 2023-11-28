548452d64a2be44f50fb098edff034b9f6419542
utkarsh-creator <utkarshtyagi959@gmail.com>
2023-11-28 15:35:10 +0530

    Minor Changes

--- client/src/Pages/NeedRoom/NeedRoom.jsx
+++ client/src/Pages/NeedRoom/NeedRoom.jsx
@@ -423,7 +423,7 @@ function NeedRoom() {
                 needRoom ? "border-b-2 border-b-[#000] font-[600]" : ""
               } pb-1`}
             >
-              Need Room
+              Need Roommates
             </span>
           </div>
           <div
@@ -440,7 +440,7 @@ function NeedRoom() {
                 !needRoom ? "border-b-2 border-b-[#000] font-[600]" : ""
               } pb-1`}
             >
-              Need Roommates
+              Need Room
             </span>
           </div>
         </div>
@@ -460,7 +460,7 @@ function NeedRoom() {
           </div>
         ) : needRoom ? (
           <div className="w-[100%]">
-            <h1 className="mb-4 mt-5">Looking for Room</h1>
+            <h1 className="mb-4 mt-5">Have Room & looking for Roommate</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 gap:4  md:gap:4 lg:gap-8">
               <div
                 style={{ gridAutoRows: "120px" }}
@@ -601,7 +601,7 @@ function NeedRoom() {
           </div>
         ) : (
           <div className="w-[100%]">
-            <h1 className="mb-4 mt-5">Have Room & looking for Roommate</h1>
+            <h1 className="mb-4 mt-5">Looking for Room</h1>
             <div className="grid grid-cols-1 md:grid-cols-2  md:gap:4 lg:gap-8">
               <div
                 style={{ gridAutoRows: "120px" }}
