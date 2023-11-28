1db77326a2b48229f28c75fb1c8af61d3908c587
Subhodeep Dey <sdeysocial@gmail.com>
2023-11-28 16:17:54 +0530

    Update NeedRoom.jsx

--- client/src/Pages/NeedRoom/NeedRoom.jsx
+++ client/src/Pages/NeedRoom/NeedRoom.jsx
@@ -610,6 +610,15 @@ function NeedRoom() {
                     className="bg-[#D9D9D9] rounded-[8px] mt-1 h-[3rem] p-4"
                   />
                 </div>
+                <div className="flex flex-col mb-6 md:mr-4">
+                  <span>Vacancy</span>
+                  <input
+                    name="remaining"
+                    value={needRoomForm["remaining"]}
+                    onChange={needRoomFormOnChangeHandler}
+                    className="bg-[#D9D9D9] rounded-[8px] mt-1 h-[3rem] p-4"
+                  />
+                </div>
               </div>
             </div>
             <div className="containerr w-[100%] mb-6 mt-6 md:mt-2">
@@ -738,15 +747,6 @@ function NeedRoom() {
                   <input
                     name="year"
                     value={needRoomMateForm["year"]}
-                    onChange={needRoomMateFormOnChangeHandler}
-                    className="bg-[#D9D9D9] rounded-[8px] mt-1 h-[3rem] p-4"
-                  />
-                </div>
-                <div className="flex flex-col mb-6 md:mr-4">
-                  <span>Vacancy</span>
-                  <input
-                    name="remaining"
-                    value={needRoomMateForm["remaining"]}
                     onChange={needRoomMateFormOnChangeHandler}
                     className="bg-[#D9D9D9] rounded-[8px] mt-1 h-[3rem] p-4"
                   />
