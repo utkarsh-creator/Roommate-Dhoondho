f0c1e90bc6b360b0110cc935786b0d8f228f9736
Subhodeep Dey <sdeysocial@gmail.com>
2023-11-28 19:31:38 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,10 @@ import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 if(process.env.NODE_ENV === "production") {
   disableReactDevTools();
+  const noOp = function() {};
+  console.log = noOp;
+  console.warn = noOp;
+  console.error = noOp;
 }
 
 const App = () => {
