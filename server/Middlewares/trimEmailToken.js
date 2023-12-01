bb81a0d45669a7545de3ed626ea7a5b0e068cc9f
Subhodeep Dey <sdeysocial@gmail.com>
2023-12-01 21:53:55 +0530

    Disabled User Deleting on production
    Disabled User Deleting on production

--- server/Middlewares/trimEmailToken.js
+++ server/Middlewares/trimEmailToken.js
@@ -1,10 +1,13 @@
 export const trimEmailToken = async (req, res, next) => {
     // Check if the response body contains the property 'emailToken'
     if (res.locals && res.locals.updatedUser && res.locals.updatedUser.emailToken) {
-        // Trim 'emailToken' from the response body
-        delete res.locals.updatedUser.emailToken;
+      // Trim 'emailToken' from the response body
+      res.locals.updatedUser = {
+        ...res.locals.updatedUser,
+        emailToken: undefined,
+      };
     }
     // Proceed to the next middleware or route handler
     next();
-};
+  };
   
--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { isAdmin } from '../Middlewares/isAdmin.js';
+import { trimEmailToken } from '../Middlewares/trimEmailToken.js';
 import { dontExecuteAtProduction } from '../Middlewares/dontExecuteAtProduction.js';
 import { deleteUser, followUser, getAllUser, getUser, getPersonalUser, UnFollowUser, updateUser, updateUserByAdmin, likeRoom, likeRoommate } from "../Controllers/UserController.js";
 
@@ -10,7 +11,7 @@ router.get('/:id', getUser)
 router.get('/personal/:id', getPersonalUser)
 router.put('/:id', updateUser)
 router.put('/admin/:id', dontExecuteAtProduction, isAdmin, updateUserByAdmin)
-router.delete('/:id', deleteUser)
+router.delete('/:id', dontExecuteAtProduction, isAdmin, deleteUser)
 router.put('/:id/follow', followUser)
 router.put('/:id/unfollow', UnFollowUser)
 router.put('/:id/likesroom', likeRoom)
