a18da75e9efd8aed70ae5cdf4f1411025da34388
Subhodeep Dey <sdeysocial@gmail.com>
2023-12-03 21:18:03 +0530

    [server] NameFormat middleware accepting spaces and dot
    NameFormat middleware accepting spaces and dot while updating name in user profile.

--- server/Middlewares/Format/nameFormat.js
+++ server/Middlewares/Format/nameFormat.js
@@ -2,7 +2,7 @@ export const nameFormat = (req, res, next) => {
     const { firstname, lastname } = req.body;
   
     // Regular expression to check if the name contains only characters
-    const nameRegex = /^[a-zA-Z]+$/;
+    const nameRegex = /^[a-zA-Z. ]+$/;
   
     if (!nameRegex.test(firstname) || !nameRegex.test(lastname)) {
       return res.status(400).json({ message: "Invalid name format. Name should contain only characters." });
